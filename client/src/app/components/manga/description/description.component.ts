e7c1518450fe4d723b790a1a3c9bbc3443f75130
tiendung69 <111dung1111@gmail.com>
2020-03-14 21:15:47 +0700

    format code

--- client/src/app/components/manga/description/description.component.ts
+++ client/src/app/components/manga/description/description.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-description',
   templateUrl: './description.component.html',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DescriptionComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router:Router,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  handleGoToMangaDetail(id){
+    this.router.navigate(['/manga/detail/'+id])
   }
 
 }
--- client/src/app/components/manga/detail/detail.component.ts
+++ client/src/app/components/manga/detail/detail.component.ts
@@ -3,10 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css']
+  styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit {
-
+  listImg = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   constructor() { }
 
   ngOnInit(): void {
