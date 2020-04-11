474f5eceb434c954df5501b31ae2e697ba23aa1e
onepiecehung <360anime.suki@gmail.com>
2020-04-11 20:07:51 +0700

    fix

--- back-end/packages/genre/genre.routes.js
+++ back-end/packages/genre/genre.routes.js
@@ -5,25 +5,25 @@ router.route("/all")
     .post(
         GenreController.autoCreateAllGenre
     )
-router.route('/')
-  .get(
-    GenreController.getAllGenre
-  );
-router.route('/:id')
-  .get(
-    GenreController.getGenReById
-  );
-router.route('/')
-  .post(
-    GenreController.createNewGenre
-  );
-router.route('/:id')
-  .put(
-    GenreController.updateGenreById
-  );
-router.route('/:id')
-  .delete(
-    GenreController.deleteGenreById
-  );
+// router.route('/')
+//   .get(
+//     GenreController.getAllGenre
+//   );
+// router.route('/:id')
+//   .get(
+//     GenreController.getGenReById
+//   );
+// router.route('/')
+//   .post(
+//     GenreController.createNewGenre
+//   );
+// router.route('/:id')
+//   .put(
+//     GenreController.updateGenreById
+//   );
+// router.route('/:id')
+//   .delete(
+//     GenreController.deleteGenreById
+//   );
 
 module.exports = router;
