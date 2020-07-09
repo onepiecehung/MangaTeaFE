825eb1952cc32d4abfde2bdf57309932d1614f2f
Dung Ha Tien <111dung1111@gmail.com>
2020-07-09 23:35:43 +0700

    set url api server and git ci deploy

--- src/app/services/manga.service.ts
+++ src/app/services/manga.service.ts
@@ -19,7 +19,7 @@ export class MangaService {
 
   loadManga(skip): Promise<ListMangaResponse> {
     return new Promise((resolve, reject) => {
-      this.apiService.getData(`${CONSTANT_API.API_ENDPOINTS.MANGA}?skip=${skip}$sort=${1}`).subscribe(response => {
+      this.apiService.getData(`${CONSTANT_API.API_ENDPOINTS.MANGA}?skip=${skip}&sort=${1}`).subscribe(response => {
         if (response.status === HTTP_STATUS.OK) {
           resolve(new ListMangaResponse(response.data));
         } else {
